fff0e5abecbc7e27c233439ea8a232128d54f57f
SergKai <sergej.kaiser@btelligent.com>
2022-06-09 10:53:49 +0200

    Add new multi select feature

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
-import streamlit 
+from operator import index
+import streamlit
 import pandas
+fruit_list_remote = """https://uni-lab-files.s3.us-west-2.amazonaws.com
+/dabw/fruit_macros.txt"""
 
 streamlit.title('My Parents new Healthy Diner')
 streamlit.header('breakfast Menu')
@@ -8,7 +11,8 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑 🍞 Hard-BoiledAvocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = (pandas.read_csv(fruit_list_remote)
+                 .set_index("Fruit")
+                 )
+streamlit.multiselect("Pick some fruits:" list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
-
-##
