523a0784540a4cee59cd806eaacaa4fb28f5bc0e
SergKai <sergej.kaiser@btelligent.com>
2022-06-09 10:34:18 +0200

    Add more content

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,5 @@
 import streamlit 
-streamlit.title('My Parents new Healthy Diner:')
+streamlit.title('My Parents new Healthy Diner')
+streamlit.header('breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
