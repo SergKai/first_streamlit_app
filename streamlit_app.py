44046372ceae087a40bbd0d4510e0c0226edc66b
SergKai <sergej.kaiser@btelligent.com>
2022-06-09 11:07:31 +0200

    Make selection active

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ FILENAME = "fruit_macros.txt"
 LOCATION = "us-west-2"
 DOMAIN = "uni-lab-files"
 CLOUD = "amazonaws"
-fruit_list_url = f"""https://{DOMAIN}.s3.{LOCATION}.{CLOUD}.com/dabw/{FILENAME}"""
+fruits_url = f"""https://{DOMAIN}.s3.{LOCATION}.{CLOUD}.com/dabw/{FILENAME}"""
 
 streamlit.title('My Parents new Healthy Diner')
 streamlit.header('breakfast Menu')
@@ -14,8 +14,12 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑 🍞 Hard-BoiledAvocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-my_fruit_list = (pandas.read_csv(fruit_list_url)
+my_fruit_list = (pandas.read_csv(fruits_url)
                  .set_index("Fruit")
                  )
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ["Avocado","Strawberries"])
+fruits_selected = (streamlit.multiselect("Pick some fruits:",
+                   list(my_fruit_list.index),
+                    ["Avocado", "Strawberries"])
+                   )
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(my_fruit_list)
